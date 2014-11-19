2b00c200c34008a072aac2401904082eb6a0dcc8
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2014-11-19 12:57:40 -0800

    Fixed loop in deduplicate tool JavaScript.

--- tool/deduplicate/app.js
+++ tool/deduplicate/app.js
@@ -3,7 +3,8 @@ $("#btnDeduplicate").click(function () {
   var lines = text.replace(/\r\n/g, "\n").split("\n");
   var found = {};
   var result = [];
-  foreach(var line in lines) {
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
     if (line != null && line.length > 0) {
       if(!found[line]) {
         result.push(line);
