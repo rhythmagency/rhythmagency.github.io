dc712d66424eb67791f3d8f271fedceef08d1c9b
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-06-15 13:20:17 -0700

    Implement hours calculation.

--- tool/calendar-total/app.js
+++ tool/calendar-total/app.js
@@ -2,11 +2,17 @@ $("#btnCalculate").click(function () {
   var text = $("#txtInput").val();
   var lines = text.replace(/\r\n/g, "\n").split("\n");
   var total = 0;
+  var hourRegex = new RegExp("\\([0-9]+(\\.[0-9]+)?hr\\)", "gi");
+  var numRegex = new RegExp("[0-9]+(\\.[0-9]+)?", "gi");
   for (var i = 0; i < lines.length; i++) {
     var line = lines[i];
-    //TODO: Get hours.
+    var matches = hourRegex.exec(line);
+    if (matches && matches.length) {
+      var strHours = numRegex.exec(matches[0]);
+      var hours = parseFloat(strHours);
+      total = total + hours;
+    }
   }
-  //TODO: Set hours.
-  var newText = "Not Yet Implemented";
+  var newText = hours.toString() + " hours";
   $("#txtOutput").val(newText);
 });
