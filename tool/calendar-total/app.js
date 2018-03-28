936a484d17cb86c6dad5e8f63d52d1546adff132
Danny Rooks <drooks@gmail.com>
2018-03-28 16:05:51 -0700

    Update txtOutputRange regex

--- tool/calendar-total/app.js
+++ tool/calendar-total/app.js
@@ -11,7 +11,7 @@ function outputHoursTotalFromTimeRanges(lines){
   //Fri Sep 16, 2016    3:45pm – 4:00pm 
   var total = 0, startHour, startMinutes, startPeriod, startDate, endHour, endMinutes, endPeriod, endDate;
   // (Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s[0-9]{1,2},\s[0-9]{4}\s{3}
-  var rangeRegex = /([0-9]{1,2})\:([0-9]{2})(am|pm)\s–\s([0-9]{1,2})\:([0-9]{2})(am|pm)/im;
+  var rangeRegex = /(([0-9]{1,2}:[0-9]{2}|[0-9]{1,2}[ap]m|[0-9]{1,2})\s-\s([0-9]{1,2}:[0-9]{2}|[0-9]{1,2}[ap]m))/im;
   
   for (var i = 0; i < lines.length; i++) {
     var line = lines[i];
