583daa6a6b204852acd3579ac58fcb5a99622135
Danny Rooks <drooks@gmail.com>
2018-03-29 15:50:08 -0700

    Update regex and time matching

--- tool/calendar-total/app.js
+++ tool/calendar-total/app.js
@@ -11,28 +11,20 @@ function outputHoursTotalFromTimeRanges(lines){
   //Fri Sep 16, 2016    3:45pm – 4:00pm 
   var total = 0, startHour, startMinutes, startPeriod, startDate, endHour, endMinutes, endPeriod, endDate;
   // (Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s[0-9]{1,2},\s[0-9]{4}\s{3}
-  var rangeRegex = /(([0-9]{1,2}:[0-9]{2}|[0-9]{1,2}[ap]m|[0-9]{1,2})\s-\s([0-9]{1,2}:[0-9]{2}|[0-9]{1,2}[ap]m))/im;
+  var rangeRegex = /(([0-9]{1,2}:[0-9]{2}|[0-9]{1,2}[ap]m|[0-9]{1,2})\s\W\s([0-9]{1,2}:[0-9]{2}|[0-9]{1,2}[ap]m))/;
   
   for (var i = 0; i < lines.length; i++) {
     var line = lines[i];
     var matches = line.match(rangeRegex);
     if (matches && matches.length) {
-      startHour = parseFloat(matches[1]);
-      startMinutes = parseFloat(matches[2]);
-      startPeriod = matches[3];
-      endHour = parseFloat(matches[4]);
-      endMinutes = parseFloat(matches[5]);
-      endPeriod = matches[6];
-
-      console.log('m', matches);
-
-      startDate = new Date('1/1/2016 ' + startHour + ':' + startMinutes  + ' ' + startPeriod);
-      endDate = new Date('1/1/2016 ' + endHour + ':' + endMinutes + ' ' + endPeriod);
+      times = line.split(" ");
+	  startDate = moment(times[0], 'HH:mma');
+	  endDate = moment(times[2], 'HH:mma');
 
       if(startDate && endDate){
         console.log('sd', startDate);
         console.log('ed', endDate);
-        total += (endDate - startDate) / 60 / 60 / 1000;
+        total += endDate.diff(startDate, 'minutes');
         console.log('total', total);
       }
 
@@ -40,7 +32,7 @@ function outputHoursTotalFromTimeRanges(lines){
       endDate = undefined;
     }
   }
-  var newText = total.toString() + " hours";
+  var newText = (total/60).toString() + " hours";
   $("#txtOutputRange").val(newText);
 }
 
