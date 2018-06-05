991d9fe45d0ba46c8a70636046ea3bf76085fe12
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2018-06-04 19:35:22 -0600

    Fix for new calendar format for time ranges.

--- tool/calendar-total/app.js
+++ tool/calendar-total/app.js
@@ -8,24 +8,42 @@ $("#btnCalculate").click(function () {
 });
 
 function outputHoursTotalFromTimeRanges(lines){
-  //Fri Sep 16, 2016    3:45pm – 4:00pm 
-  var total = 0, startHour, startMinutes, startPeriod, startDate, endHour, endMinutes, endPeriod, endDate;
-  // (Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s[0-9]{1,2},\s[0-9]{4}\s{3}
-  var rangeRegex = /(([0-9]{1,2}:[0-9]{2}|[0-9]{1,2}[ap]m|[0-9]{1,2})\s\W\s([0-9]{1,2}:[0-9]{2}|[0-9]{1,2}[ap]m))/;
+  //12 – 1pm
+  //2 – 6pm
+  //9 – 10am
+  //9am – 1pm
+  //10 – 10:30am
+  var total = 0, startHour, startMinutes, startPeriod, startDate, endHour, endMinutes, endPeriod, endDate,
+    startText, endText, startAmPm, endAmPm, hasStartAmPm, hasEndAmPm;
+  var rangeRegex = /^([0-9]{1,2}(\:[0-9]{2})?(am|pm)?)\s(\u2013|-)\s([0-9]{1,2}(\:[0-9]{2})?(am|pm)?)(?=$)/;
+  var amPm = /(am|pm)(?=$)/;
   
   for (var i = 0; i < lines.length; i++) {
     var line = lines[i];
     var matches = line.match(rangeRegex);
     if (matches && matches.length) {
       times = line.split(" ");
-	  startDate = moment(times[0], 'HH:mma');
-	  endDate = moment(times[2], 'HH:mma');
+      startText = times[0];
+      endText = times[2];
+      startAmPm = startText.match(amPm);
+      endAmPm = endText.match(amPm);
+      hasStartAmPm = startAmPm && startAmPm.length;
+      hasEndAmPm = endAmPm && endAmPm.length;
+      if (!hasStartAmPm && !hasEndAmPm) {
+        startText = startText + "am";
+        endText = endText = "pm";
+      }
+      if (hasStartAmPm && !hasEndAmPm) {
+        endText = endText + startAmPm[0];
+      }
+      if (!hasStartAmPm && hasEndAmPm) {
+        startText = startText + endAmPm[0];
+      }
+      startDate = moment(startText, 'HH:mma');
+      endDate = moment(endText, 'HH:mma');
 
-      if(startDate && endDate){
-        console.log('sd', startDate);
-        console.log('ed', endDate);
+      if(startDate && endDate) {
         total += endDate.diff(startDate, 'minutes');
-        console.log('total', total);
       }
 
       startDate = undefined;
