e00ae8e0ecc8b7ec963eb53f75fd118a3ddc3b16
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2019-06-12 22:50:19 -0600

    Create app.js

---
+++ sandbox/nick/template-dom/app.js
@@ -0,0 +1,42 @@
+function getData() {
+    var items = [{
+        name: "Bob",
+        bio: "Bob works with apples."
+    }, {
+        name: "Jane",
+        bio: "Jane works with deer."
+    }];
+    return (items);
+}
+
+function renderCell(item) {
+    let compiler = new Compiler();
+    let compiled = compiler.compile(`
+        <div class="grid__item">
+            <h2 ${compiler.namedElement("Header")}>${item.name}</h2>
+            <p ${compiler.namedElement("Bio")}>${item.bio}</p>
+        </div>
+    `);
+    let {Header, Bio} = compiler;
+    Header.style.backgroundColor = "#f00";
+    Bio.style.backgroundColor = "#0f0";
+    return compiled;
+}
+
+function renderResults() {
+    let compiler = new Compiler();
+    let items = getData().map(renderCell);
+    let compiled = compiler.compile(`
+        <div class="grid">
+            ${compiler.nestElements("grid-items", items)}
+        </div>
+    `);
+    return (compiled);
+}
+
+function renderAll() {
+    let results = renderResults();
+    document.body.appendChild(results);
+}
+
+renderAll();
